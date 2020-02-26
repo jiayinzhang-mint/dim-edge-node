9f30e66c69248dbd5059a6fa66811dce67cf8090
MINT <mint@insdim.com>
2020-02-26 17:59:39 +0800

    query api

--- store/query.go
+++ store/query.go
@@ -11,7 +11,21 @@ import (
 func (i *Influx) InsertData(metrics *[]influxdb.Metric, bucket string, org string) (count int, err error) {
 	count, err = i.GetDB().Write(context.Background(), "dim-edge", "INSDIM", *metrics...)
 	if err != nil {
-		logrus.Fatal(err) // as above use your own error handling here.
+		logrus.Fatal(err)
+	}
+
+	return
+}
+
+// QueryData query data
+func (i *Influx) QueryData(queryString string, org string) (res *influxdb.QueryCSVResult, err error) {
+
+	res, err = i.GetDB().QueryCSV(context.Background(),
+		queryString,
+		org)
+	if err != nil {
+		logrus.Error(err)
+		return
 	}
 
 	return
--- store/query_test.go
+++ store/query_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-func TestQuery(*testing.T) {
+func TestInsertData(*testing.T) {
 	influx := &Influx{
 		Address: "http://127.0.0.1:9999",
 		Token:   "4oXjSoIuU1F3A1zu-xYp0eJ9q_vsLQmtDPPTNuDnrs7R7H7qGAQ1GNaX4hNtJKx5ZRfnoj_TW5Uwe5NJUBvLOA==",
@@ -32,4 +32,52 @@ func TestQuery(*testing.T) {
 		)}, "dim-edge", "INSDIM")
 
 	logrus.Info(c, err)
+}
+
+func TestQuery(*testing.T) {
+	influx := &Influx{
+		Address: "http://127.0.0.1:9999",
+		Token:   "4oXjSoIuU1F3A1zu-xYp0eJ9q_vsLQmtDPPTNuDnrs7R7H7qGAQ1GNaX4hNtJKx5ZRfnoj_TW5Uwe5NJUBvLOA==",
+	}
+
+	err := influx.ConnectToDB()
+	if err != nil {
+		logrus.Error(err)
+	}
+
+	// Query
+	res, queryErr := influx.QueryData(
+		`from(bucket: "dim-edge")
+  		|> range(start: -10h)
+  		|> filter(fn: (r)=>
+				r._field == "cpu" and
+				r._measurement == "system-metrics" and
+				r.hostname == "hal9000"
+			)`,
+		"INSDIM")
+	if queryErr != nil {
+		logrus.Error(err)
+	}
+
+	// Marshal data
+	type influxRecord struct {
+		Zone   ***string   `flux:"name" json:"zone"`
+		Stop   time.Time   `flux:"_stop" json:"-"`
+		Start  time.Time   `flux:"_start" json:"-"`
+		Time   time.Time   `flux:"_time" json:"date"`
+		HostIP string      `flux:"host_ip" json:"-"`
+		Count  interface{} `flux:"_value" json:"count"`
+	}
+
+	var r influxRecord
+	for res.Next() {
+		mErr := res.Unmarshal(&r)
+		logrus.Info(r)
+		if mErr != nil {
+			logrus.Error(mErr)
+		}
+	}
+
+	// Close DB
+	influx.GetDB().Close()
 }
